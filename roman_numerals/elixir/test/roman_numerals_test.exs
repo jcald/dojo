f9243d7971cd00db17c7e45dc58cbd980f38535a
Francesco Rodriguez <lrodriguezsanc@gmail.com>
2012-06-30 03:43:38 -0500

    some updates to elixir

--- roman_numerals/elixir/test/roman_numerals_test.exs
+++ roman_numerals/elixir/test/roman_numerals_test.exs
@@ -1,9 +1,10 @@
 Code.require_file "../test_helper", __FILE__
 
 defmodule RomanNumeralsTest do
-	use ExUnit::Case
+	use ExUnit.Case
 
 	test "assert the truth" do
-		assert true
+		assert RomanNumerals.to_roman(1) == "I"
+		assert RomanNumerals.to_roman(2) == "II"
 	end
 end
